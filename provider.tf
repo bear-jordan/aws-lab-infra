907258e73a42262e76de1fc720da70fae88d8561
bear-jordan <bearjordan@gmail.com>
2025-04-12 15:17:19 -0400

    FIX: updated provider

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "bear-lab-terraform-state"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
+  backend "s3" { }
 }
