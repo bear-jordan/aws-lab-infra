f9fba206994d9f2344242422b3a4bc53a4264e01
bear-jordan <bearjordan@gmail.com>
2025-04-13 19:10:06 -0400

    ENH: add vm role

--- iam.tf
+++ iam.tf
@@ -3,3 +3,31 @@ resource "aws_iam_user_policy_attachment" "query_editor_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonRedshiftQueryEditorV2FullAccess"
 }
 
+resource "aws_iam_role" "vmimport" {
+  name = "vmimport"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "vmie.amazonaws.com"
+        },
+        Action = "sts:AssumeRole",
+        Condition = {
+          StringEquals = {
+            "sts:ExternalId" : "vmimport"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = merge(
+    var.default_tags,
+    {
+      Name = "vmimport"
+    }
+  )
+}
