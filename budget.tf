b3c4ec6cd3679ae25c444e0700efc80c09d999b5
bear-jordan <bearjordan@gmail.com>
2025-05-03 17:25:54 -0400

    ENH: update budget to 40

--- budget.tf
+++ budget.tf
@@ -1,7 +1,7 @@
 resource "aws_budgets_budget" "aws_lab_budget" {
   name         = "MonthlyBudget"
   budget_type  = "COST"
-  limit_amount = "10"
+  limit_amount = "40"
   limit_unit   = "USD"
   time_unit    = "MONTHLY"
 
