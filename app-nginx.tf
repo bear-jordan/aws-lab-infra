e298bc8cb64f402dc73ba7ed8c6cc0c2ed9ee4e4
bear-jordan <bearjordan@gmail.com>
2025-05-03 18:10:27 -0400

    MAINT: removed unused images

--- app-nginx.tf
+++ app-nginx.tf
@@ -1,3 +1,30 @@
+resource "aws_security_group" "aws_lab_nginx_sg" {
+  name        = "aws_lab_nginx_sg"
+  description = "Nginx security group"
+  vpc_id      = aws_vpc.aws_lab_vpc.id
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.default_tags,
+    {
+      Name = "aws_lab_nginx_sg"
+    }
+  )
+}
+
 resource "aws_instance" "hello_world_0" {
   ami           = "ami-0b0ea68c435eb488d"
   instance_type = "t2.micro"
@@ -13,44 +40,6 @@ resource "aws_instance" "hello_world_0" {
              echo '<!doctype html>
              <html lang="en"><h1>Home page!</h1></br>
              <h3>(Instance 0)</h3>
-             </html>' | sudo tee /var/www/html/index.html
-             EOF
-}
-
-resource "aws_instance" "hello_world_1" {
-  ami           = "ami-0b0ea68c435eb488d"
-  instance_type = "t2.micro"
-
-  subnet_id = aws_subnet.public_subnet_1.id
-
-  user_data = <<-EOF
-             #!/bin/bash
-             sudo apt-get update
-             sudo apt-get install -y nginx
-             sudo systemctl start nginx
-             sudo systemctl enable nginx
-             echo '<!doctype html>
-             <html lang="en"><h1>Home page 1!</h1></br>
-             <h3>(Instance 1)</h3>
-             </html>' | sudo tee /var/www/html/index.html
-             EOF
-}
-
-resource "aws_instance" "hello_world_2" {
-  ami           = "ami-0b0ea68c435eb488d"
-  instance_type = "t2.micro"
-
-  subnet_id = aws_subnet.public_subnet_2.id
-
-  user_data = <<-EOF
-             #!/bin/bash
-             sudo apt-get update
-             sudo apt-get install -y nginx
-             sudo systemctl start nginx
-             sudo systemctl enable nginx
-             echo '<!doctype html>
-             <html lang="en"><h1>Home page!</h1></br>
-             <h3>(Instance 2)</h3>
              </html>' | sudo tee /var/www/html/index.html
              EOF
 }
