db47e8aa310e896ea02296a3206a4901241a4db4
bear-jordan <bearjordan@gmail.com>
2025-05-03 18:35:06 -0400

    FIX: update security permissions for nginx

--- app-nginx.tf
+++ app-nginx.tf
@@ -4,10 +4,17 @@ resource "aws_security_group" "aws_lab_nginx_sg" {
   vpc_id      = aws_vpc.aws_lab_vpc.id
 
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
