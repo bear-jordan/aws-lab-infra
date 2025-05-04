41de448aa3a82fbbd3181f604a35f9b3af5fdf53
bear-jordan <bearjordan@gmail.com>
2025-05-03 21:35:35 -0400

    MAINT: removing 443 from alb listener

--- alb.tf
+++ alb.tf
@@ -47,13 +47,6 @@ resource "aws_security_group" "aws_lab_alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
